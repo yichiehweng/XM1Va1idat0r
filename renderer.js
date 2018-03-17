bd288e3fac1e7e75363b5d17396874004ceaddfc
YiChiehWeng <yichieh.weng@gmail.com>
2018-03-18 01:56:45 +0800

    add file select function

--- renderer.js
+++ renderer.js
@@ -1,13 +1,33 @@
 var fs= require('file-system');
+
+var app = require('electron').remote; 
+var dialog = app.dialog;
+
 var xml2js= require('xml2js');
 var parser=new xml2js.Parser();
 var PatientController = require("./controller/PatientController");
-var MakeUL=require("./util/makeUL")
-
+var MakeUL=require("./util/makeUL");
+var filePath=document.getElementById("filePath");
+var openFile=document.getElementById("openFile");   
 var btn_upload=document.getElementById("btn_upload");
 
+openFile.addEventListener('click',function(){
+    dialog.showOpenDialog({ filters: [
+        { name: 'xml', extensions: ['xml'] }
+       ]}, function (fileNames) {
+       if (fileNames === undefined) return;
+       var fileName = fileNames[0];
+       
+       filePath.value=fileName;
+      }); 
+});
+
+
 btn_upload.addEventListener('click',function(){
-    var xmlfile = __dirname + "/public/xmlfiles/patient.xml";
+
+    //var xmlfile = __dirname + "/public/xmlfiles/patient.xml";
+    var xmlfile=filePath.value; 
+
     fs.readFile(xmlfile,'utf-8',function (error, xml){
         if (error) {
             console.log(error);
